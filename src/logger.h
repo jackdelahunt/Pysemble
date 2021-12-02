26cf2cb73bebbece816373062bc5cacb3f163667
jackdelahunt <jdelahun@redhat.com>
2021-12-02 19:25:38 +0000

    Refactor: removed non library code

--- src/logger.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <stdio.h>
-
-void log_info(const char* message);
-void log_warn(const char* message);
-void log_err(const char* message);
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "logger.h"
-
-int main() {
-    log_info("Capacitor is at 5%");
-    log_warn("Capacitor is low");
-    log_err("Capacitor is empty");
-    return 0;
-}
