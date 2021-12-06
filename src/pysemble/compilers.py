890862dda5e666f06abf536487b7551254513b3b
jackdelahunt <jdelahun@redhat.com>
2021-12-06 09:51:13 +0000

    Feat: Clang and GCC support

--- src/pysemble/compilers.py
+++ src/pysemble/compilers.py
@@ -18,15 +18,15 @@ class Compiler:
     def build_shared_object(self, out: Path, files: list[str]) -> str:
         pass
 
+class GCC_API(Compiler):
 
-class Gpp(Compiler):
-
-    def __init__(self, version: int = 17):
-        self.version: str = "c++" + str(version)
+    def __init__(self, compiler_name: str, version: str):
+        self.compiler_name = compiler_name
+        self.version = version
 
     def build(self, out: Path, files: list[str], includes: list[str] = [], link_path: str = "", shared_objects: list[str] = [], static_libraries: list[str]=[]):
         file_list = concat_list(files)
-        final_command = "g++ -std=" + self.version + " -o " + str(out) + " " + file_list + " "
+        final_command = self.compiler_name + " -std=" + self.version + " -o " + str(out) + " " + file_list + " "
 
         if len(includes) > 0:
             for i in includes:
@@ -43,7 +43,7 @@ class Gpp(Compiler):
             for ar in static_libraries:
                 final_command += ar + " "
 
-        log("\ng++ building " + file_list + " -> " + str(out) + " with:\n"
+        log(self.compiler_name + " building " + file_list + " -> " + str(out) + " with:\n"
             "Includes: " + str(includes) + "\n"
             "Link path: " + link_path + "\n"
             "Link libraries: " + str(shared_objects) + "\n"
@@ -59,13 +59,13 @@ class Gpp(Compiler):
             # base file is the file name
             base_file: str = os.path.basename(file)
             object_file = str(location / (base_file + ".o"))
-            final_command = "g++ -std=" + self.version + " -c -o " + object_file + " " + file
+            final_command = self.compiler_name + " -std=" + self.version + " -c -o " + object_file + " " + file
 
             if len(includes) > 0:
                 for i in includes:
                     final_command += " -I" + i + " "
 
-            log("g++ building the following as an object file: " + base_file + " -> " + object_file, info=True)
+            log(self.compiler_name + " building the following as an object file: " + base_file + " -> " + object_file, info=True)
             log(final_command, debug=True)
             os.system(final_command)
             object_files.append(object_file)
@@ -77,18 +77,31 @@ class Gpp(Compiler):
         for file in files:
             base_file: str = os.path.basename(file)
             object_file = str(location / (base_file + ".o"))
-            log("g++ building the following as sharable object files: " + base_file + " -> " + object_file, info=True)
-            os.system("g++ -std=" + self.version + " -c " + file + " -o " + object_file + " -fpic")
+            log(self.compiler_name + " building the following as sharable object files: " + base_file + " -> " + object_file, info=True)
+            os.system(self.compiler_name + " -std=" + self.version + " -c " + file + " -o " + object_file + " -fpic")
             object_files.append(object_file)
         return object_files
 
     def build_shared_object(self, out: Path, files: list[str]) -> str:
-        final_command = "g++ -std=" + self.version + " -shared "
+        final_command = self.compiler_name + " -std=" + self.version + " -shared "
         out_str = str(out)
         file_list = concat_list(files)
         final_command += file_list
         final_command += " -o " + out_str
-        log("g++ building the following as a shared object: " + file_list + " -> " + out_str, info=True)
+        log(self.compiler_name + " building the following as a shared object: " + file_list + " -> " + out_str, info=True)
         log(final_command, debug=True)
         os.system(final_command)
         return out_str
+
+class Gcc(GCC_API):
+    def __init__(self, version: str):
+        super().__init__("gcc", version)
+
+class Gpp(GCC_API):
+    def __init__(self, version: str):
+        super().__init__("g++", version)
+
+
+class Clang(GCC_API):
+    def __init__(self, version: str):
+        super().__init__("clang", version)
