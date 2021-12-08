1ea36c1daa08eec1435e80f0daccf0b5bb8cddd8
jackdelahunt <jdelahun@redhat.com>
2021-12-08 11:08:12 +0000

    Feat can now set optimisation level

--- src/pysemble/compilers.py
+++ src/pysemble/compilers.py
@@ -18,15 +18,20 @@ class Compiler:
     def build_shared_object(self, out: Path, files: list[str]) -> str:
         pass
 
+
 class GCC_API(Compiler):
 
-    def __init__(self, compiler_name: str, version: str):
+    def __init__(self, compiler_name: str, version: str, optimization: int):
         self.compiler_name = compiler_name
-        self.version = version
+        self.flags: list[str] = ["-std=" + version, "-O" + str(optimization)]
 
     def build(self, out: Path, files: list[str], includes: list[str] = [], link_path: str = "", shared_objects: list[str] = [], static_libraries: list[str]=[]):
         file_list = concat_list(files)
-        final_command = self.compiler_name + " -std=" + self.version + " -o " + str(out) + " " + file_list + " "
+        flag_list = concat_list(self.flags)
+        final_command = self.compiler_name + \
+                        flag_list + \
+                        " -o " + str(out) + " " \
+                        + file_list + " "
 
         if len(includes) > 0:
             for i in includes:
@@ -38,7 +43,6 @@ class GCC_API(Compiler):
         if len(shared_objects) > 0:
             for so in shared_objects:
                 final_command += "-l" + so + " "
-
 
         if len(static_libraries) > 0:
             for ar in static_libraries:
@@ -60,7 +64,7 @@ class GCC_API(Compiler):
             # base file is the file name
             base_file: str = os.path.basename(file)
             object_file = str(location / (base_file + ".o"))
-            final_command = self.compiler_name + " -std=" + self.version + " -c -o " + object_file + " " + file
+            final_command = self.compiler_name + concat_list(self.flags) + " -c -o " + object_file + " " + file
 
             if len(includes) > 0:
                 for i in includes:
@@ -79,29 +83,36 @@ class GCC_API(Compiler):
             base_file: str = os.path.basename(file)
             object_file = str(location / (base_file + ".o"))
             log(self.compiler_name + " building the following as sharable object files: " + base_file + " -> " + object_file, info=True)
-            os.system(self.compiler_name + " -std=" + self.version + " -c " + file + " -o " + object_file + " -fpic")
+            os.system(self.compiler_name + concat_list(self.flags) + " -c " + file + " -o " + object_file + " -fpic")
             object_files.append(object_file)
         return object_files
 
     def build_shared_object(self, out: Path, files: list[str]) -> str:
-        final_command = self.compiler_name + " -std=" + self.version + " -shared "
+        final_command = self.compiler_name + concat_list(self.flags) + " -shared "
         out_str = str(out)
         file_list = concat_list(files)
         final_command += file_list
         final_command += " -o " + out_str
-        log(self.compiler_name + " building the following as a shared object: " + file_list + " -> " + out_str, info=True)
+        log(self.compiler_name + " building the following as a shared object: " + file_list + " -> " + out_str,
+            info=True)
         log(final_command, debug=True)
         os.system(final_command)
         return out_str
 
+
 class Gcc(GCC_API):
-    def __init__(self, version: str = "c++17"):
-        super().__init__("gcc", version)
+    def __init__(self, version: str = "c++17", optimization: int = 2):
+        if optimization < 0 or optimization > 4:
+            log("Optimization level needs to be between 0 -> 4", err=True)
+
+        super().__init__("gcc", version, optimization)
+
 
 class Gpp(GCC_API):
-    def __init__(self, version: str = "c++17"):
-        super().__init__("g++", version)
+    def __init__(self, version: str = "c++17", optimization: int = 2):
+        super().__init__("g++", version, optimization)
+
 
 class Clang(GCC_API):
-    def __init__(self, version: str = "c++17"):
-        super().__init__("clang", version)
+    def __init__(self, version: str = "c++17", optimization: int = 2):
+        super().__init__("clang", version, optimization)
